a1728cc78833aa90b7017171b10a6d4f01bd64bc
m-ahmedbashir <93282144a@gmail.com>
2022-11-11 22:30:26 +0500

    Task 1 JS DOM paragraph Style

---
+++ Task 1/js/index.js
@@ -0,0 +1,5 @@
+function js_style() {
+  document.getElementById("text").style.fontSize = "33px";
+  document.getElementById("text").style.fontFamily = "Courier New";
+  document.getElementById("text").style.color = "red";
+}
